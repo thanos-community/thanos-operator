4a83b831f0829b5ac8f7afdc58bf056bb6db2b67
Saswata Mukherjee <saswataminsta@yahoo.com>
2024-06-07 12:44:12 +0100

    Add initial spec for Query
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

---
+++ api/v1alpha1/thanosquery_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ThanosQuerySpec defines the desired state of ThanosQuery
+type ThanosQuerySpec struct {
+	CommonThanosFields `json:",inline"`
+
+	// Querier replica labels to configure.
+	QuerierReplicaLabels []string `json:"replicaLabels,omitempty"`
+
+	// Override default StoreAPI selector labels, to select specific StoreAPIs to query.
+	CustomStoreLabelSelector *metav1.LabelSelector `json:"storeLabelSelector,omitempty"`
+}
+
+// ThanosQueryStatus defines the observed state of ThanosQuery
+type ThanosQueryStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ThanosQuery is the Schema for the thanosqueries API
+type ThanosQuery struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ThanosQuerySpec   `json:"spec,omitempty"`
+	Status ThanosQueryStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ThanosQueryList contains a list of ThanosQuery
+type ThanosQueryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ThanosQuery `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ThanosQuery{}, &ThanosQueryList{})
+}
