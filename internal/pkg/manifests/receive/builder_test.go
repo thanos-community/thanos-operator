e3fdd98c438b0248b4a21b55ad2449d83cd5e51b
Philip Gough <philip.p.gough@gmail.com>
2024-06-20 09:34:19 +0100

    test:manifests: Unit tests for receive hashring builders

--- internal/pkg/manifests/receive/builder_test.go
+++ internal/pkg/manifests/receive/builder_test.go
@@ -1,12 +1,21 @@
 package receive
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/thanos-community/thanos-operator/internal/pkg/manifests"
 
+	"github.com/go-logr/logr/testr"
+
+	corev1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/diff"
 	"k8s.io/utils/ptr"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -217,6 +226,564 @@ func TestNewIngestorService(t *testing.T) {
 
 			if ingester.Spec.ClusterIP != "None" {
 				t.Errorf("expected ingester service to have ClusterIP 'None', got %s", ingester.Spec.ClusterIP)
+			}
+		})
+	}
+}
+
+func TestNewRouterService(t *testing.T) {
+	opts := RouterOptions{
+		Options: manifests.Options{
+			Name:      "test",
+			Namespace: "ns",
+			Labels: map[string]string{
+				"some-custom-label":      someCustomLabelValue,
+				"some-other-label":       someOtherLabelValue,
+				"app.kubernetes.io/name": "expect-to-be-discarded",
+			},
+		},
+	}
+
+	for _, tc := range []struct {
+		name string
+		opts RouterOptions
+	}{
+		{
+			name: "test router service correctness",
+			opts: opts,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.opts.Options = tc.opts.ApplyDefaults()
+			router := NewRouterService(tc.opts)
+			if router.GetName() != tc.opts.Name {
+				t.Errorf("expected router service name to be %s, got %s", tc.opts.Name, router.GetName())
+			}
+			if router.GetNamespace() != tc.opts.Namespace {
+				t.Errorf("expected router service namespace to be %s, got %s", tc.opts.Namespace, router.GetNamespace())
+			}
+			// ensure we inherit the labels from the Options struct and that the strict labels cannot be overridden
+			if len(router.GetLabels()) != 7 {
+				t.Errorf("expected router service to have 7 labels, got %d", len(router.GetLabels()))
+			}
+			// ensure custom labels are set
+			if router.GetLabels()["some-custom-label"] != someCustomLabelValue {
+				t.Errorf("expected router service to have label 'some-custom-label' with value 'xyz', got %s", router.GetLabels()["some-custom-label"])
+			}
+			if router.GetLabels()["some-other-label"] != someOtherLabelValue {
+				t.Errorf("expected router service to have label 'some-other-label' with value 'abc', got %s", router.GetLabels()["some-other-label"])
+			}
+			// ensure default labels are set
+			expect := labelsForRouter(tc.opts.Options)
+			for k, v := range expect {
+				if router.GetLabels()[k] != v {
+					t.Errorf("expected router service to have label %s with value %s, got %s", k, v, router.GetLabels()[k])
+				}
+			}
+
+		})
+	}
+}
+
+func TestBuildHashrings(t *testing.T) {
+	logger := testr.New(t)
+	baseOptions := manifests.Options{
+		Name:      "test",
+		Namespace: "test",
+		Replicas:  3,
+	}
+
+	for _, tc := range []struct {
+		name        string
+		passedState Hashrings
+		opts        func() HashringOptions
+		expect      client.Object
+	}{
+		{
+			name:        "test result when no state is passed",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: EmptyHashringConfig,
+				},
+			},
+		},
+		{
+			name:        "test result when no previous state is passed but we have new state with missing owner reference",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"test": {
+							DesiredReplicasReplicas: 2,
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+											{
+												Addresses: []string{"b"},
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: EmptyHashringConfig,
+				},
+			},
+		},
+		{
+			name:        "test result when no previous state is passed but we have new state with mismatched owner reference",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"test": {
+							DesiredReplicasReplicas: 2,
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "some-other-resource",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Hostname:  ptr.To("a"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+											{
+												Addresses: []string{"b"},
+												Hostname:  ptr.To("b"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: EmptyHashringConfig,
+				},
+			},
+		},
+		{
+			name:        "test result when no previous state is passed but we have new state which is not ready",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"test": {
+							DesiredReplicasReplicas: 2,
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "test",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Hostname:  ptr.To("a"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+											{
+												Addresses: []string{"b"},
+												Hostname:  ptr.To("b"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(false),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: EmptyHashringConfig,
+				},
+			},
+		},
+		{
+			name:        "test result when no previous state is passed but we have new state which is ready",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"test": {
+							DesiredReplicasReplicas: 2,
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											Namespace: "test",
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "test",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Hostname:  ptr.To("a"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+											{
+												Addresses: []string{"b"},
+												Hostname:  ptr.To("b"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: `[
+    {
+        "endpoints": [
+            {
+                "address": "a.test.test.svc.cluster.local:19291",
+                "az": ""
+            },
+            {
+                "address": "b.test.test.svc.cluster.local:19291",
+                "az": ""
+            }
+        ]
+    }
+]`,
+				},
+			},
+		},
+		{
+			name:        "test result when tenants are set",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"test": {
+							DesiredReplicasReplicas: 2,
+							Tenants:                 []string{"foobar"},
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											Namespace: "test",
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "test",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Hostname:  ptr.To("a"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+											{
+												Addresses: []string{"b"},
+												Hostname:  ptr.To("b"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: `[
+    {
+        "tenants": [
+            "foobar"
+        ],
+        "endpoints": [
+            {
+                "address": "a.test.test.svc.cluster.local:19291",
+                "az": ""
+            },
+            {
+                "address": "b.test.test.svc.cluster.local:19291",
+                "az": ""
+            }
+        ]
+    }
+]`,
+				},
+			},
+		},
+		{
+			name:        "test result with multiple hashrings where tenants are set",
+			passedState: nil,
+			opts: func() HashringOptions {
+				return HashringOptions{
+					Options: baseOptions,
+					HashringSettings: map[string]HashringMeta{
+						"a": {
+							DesiredReplicasReplicas: 2,
+							Tenants:                 []string{"foobar"},
+							Priority:                1,
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											Namespace: "test",
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "a",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"a"},
+												Hostname:  ptr.To("a"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+											{
+												Addresses: []string{"a1"},
+												Hostname:  ptr.To("a1"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"b": {
+							DesiredReplicasReplicas: 2,
+							TenantMatcherType:       TenantMatcherGlob,
+							Tenants:                 []string{"baz*"},
+							AssociatedEndpointSlices: discoveryv1.EndpointSliceList{
+								Items: []discoveryv1.EndpointSlice{
+									{
+										ObjectMeta: metav1.ObjectMeta{
+											Namespace: "test",
+											OwnerReferences: []metav1.OwnerReference{
+												{
+													Name: "b",
+													Kind: "Service",
+												},
+											},
+										},
+										Endpoints: []discoveryv1.Endpoint{
+											{
+												Addresses: []string{"b"},
+												Hostname:  ptr.To("b"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+											{
+												Addresses: []string{"b1"},
+												Hostname:  ptr.To("b1"),
+												Conditions: discoveryv1.EndpointConditions{
+													Ready: ptr.To(true),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+			},
+			expect: &corev1.ConfigMap{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "ConfigMap",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: "test",
+					Labels:    labelsForRouter(baseOptions),
+				},
+				Data: map[string]string{
+					HashringConfigKey: `[
+    {
+        "tenants": [
+            "foobar"
+        ],
+        "endpoints": [
+            {
+                "address": "a.a.test.svc.cluster.local:19291",
+                "az": ""
+            },
+            {
+                "address": "a1.a.test.svc.cluster.local:19291",
+                "az": ""
+            }
+        ]
+    },
+    {
+        "tenants": [
+            "baz*"
+        ],
+        "tenant_matcher_type": "glob",
+        "endpoints": [
+            {
+                "address": "b.b.test.svc.cluster.local:19291",
+                "az": ""
+            },
+            {
+                "address": "b1.b.test.svc.cluster.local:19291",
+                "az": ""
+            }
+        ]
+    }
+]`,
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var cm *corev1.ConfigMap
+			if tc.passedState != nil {
+				conf, err := tc.passedState.toJson()
+				if err != nil {
+					t.Fatalf("failed to marshal passed state: %v", err)
+				}
+				cm.Data = map[string]string{
+					HashringConfigKey: conf,
+				}
+			}
+
+			got, _ := BuildHashrings(logger, cm, tc.opts())
+			if got == nil {
+				t.Errorf("expected BuildHashrings to return a ConfigMap, got nil")
+			}
+
+			if !equality.Semantic.DeepEqual(got, tc.expect) {
+				fmt.Println(diff.ObjectDiff(tc.expect, got))
+				t.Errorf("expected BuildHashrings to return a ConfigMap with data \n%v, \ngot \n%v", tc.expect, got)
 			}
 		})
 	}
