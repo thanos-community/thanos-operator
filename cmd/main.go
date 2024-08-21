cec64543f54f386c8d92be46926f4a19c21d8f80
Philip Gough <philip.p.gough@gmail.com>
2024-08-21 13:21:49 +0100

    Do not instantiate second store reconciler

--- cmd/main.go
+++ cmd/main.go
@@ -191,13 +191,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ThanosCompact")
 		os.Exit(1)
 	}
-	if err = (&controller.ThanosStoreReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "ThanosStore")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
