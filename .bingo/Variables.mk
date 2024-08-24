61aeca554ee55fa028a4c5fd17e05f2a0f826e70
Saswata Mukherjee <saswataminsta@yahoo.com>
2024-08-24 10:00:53 +0100

    Bump k8s.io/, sigs.k8s.io/ and tooling for K8s 1.31 (#74)
    * Bump k8s.io/, sigs.k8s.io/ and tooling for K8s 1.31
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Bump kind in CI
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    ---------
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -29,9 +29,9 @@ $(GOIMPORTS): $(BINGO_DIR)/goimports.mod
 	@echo "(re)installing $(GOBIN)/goimports-v0.20.0"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=goimports.mod -o=$(GOBIN)/goimports-v0.20.0 "golang.org/x/tools/cmd/goimports"
 
-GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.58.0
+GOLANGCI_LINT := $(GOBIN)/golangci-lint-v1.59.1
 $(GOLANGCI_LINT): $(BINGO_DIR)/golangci-lint.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/golangci-lint-v1.58.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.58.0 "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	@echo "(re)installing $(GOBIN)/golangci-lint-v1.59.1"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=golangci-lint.mod -o=$(GOBIN)/golangci-lint-v1.59.1 "github.com/golangci/golangci-lint/cmd/golangci-lint"
 
