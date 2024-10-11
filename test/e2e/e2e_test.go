81c9118ed757384497c06498133a3a9717b91fd3
Philip Gough <philip.p.gough@gmail.com>
2024-10-11 14:10:48 +0100

    test:e2e: Attempt run in parallel

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -19,14 +19,11 @@ package e2e
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/exec"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/thanos-community/thanos-operator/api/v1alpha1"
 	"github.com/thanos-community/thanos-operator/internal/controller"
 	"github.com/thanos-community/thanos-operator/internal/pkg/manifests"
@@ -35,18 +32,12 @@ import (
 	"github.com/thanos-community/thanos-operator/internal/pkg/manifests/store"
 	"github.com/thanos-community/thanos-operator/test/utils"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/utils/ptr"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
 const (
@@ -66,145 +57,8 @@ const (
 	hashringName = "default"
 )
 
-var _ = Describe("controller", Ordered, func() {
-	var c client.Client
-
-	BeforeAll(func() {
-		logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
-		By("installing prometheus operator")
-		Expect(utils.InstallPrometheusOperator()).To(Succeed())
-
-		By("installing the cert-manager")
-		Expect(utils.InstallCertManager()).To(Succeed())
-
-		By("creating manager namespace")
-		cmd := exec.Command("kubectl", "create", "ns", namespace)
-		_, _ = utils.Run(cmd)
-
-		By("install MinIO")
-		Expect(utils.InstallMinIO()).To(Succeed())
-
-		By("create secret")
-		Expect(utils.CreateMinioObjectStorageSecret()).To(Succeed())
-
-		scheme := runtime.NewScheme()
-		if err := v1alpha1.AddToScheme(scheme); err != nil {
-			fmt.Println("failed to add scheme")
-			os.Exit(1)
-		}
-		if err := appsv1.AddToScheme(scheme); err != nil {
-			fmt.Println("failed to add scheme")
-			os.Exit(1)
-		}
-		if err := corev1.AddToScheme(scheme); err != nil {
-			fmt.Println("failed to add scheme")
-			os.Exit(1)
-		}
-		if err := monitoringv1.AddToScheme(scheme); err != nil {
-			fmt.Println("failed to add scheme")
-			os.Exit(1)
-		}
-		if err := rbacv1.AddToScheme(scheme); err != nil {
-			fmt.Println("failed to add scheme")
-			os.Exit(1)
-		}
-
-		cl, err := client.New(config.GetConfigOrDie(), client.Options{
-			Scheme: scheme,
-		})
-		if err != nil {
-			fmt.Println("failed to create client")
-			os.Exit(1)
-		}
-		c = cl
-
-		By("Setup prometheus")
-		Expect(utils.SetUpPrometheus(c)).To(Succeed())
-	})
-
-	AfterAll(func() {
-		By("uninstalling the Prometheus manager bundle")
-		utils.UninstallPrometheusOperator()
-
-		By("uninstalling the cert-manager bundle")
-		utils.UninstallCertManager()
-
-		By("uninstalling MinIO")
-		utils.UninstallMinIO()
-
-		By("removing manager namespace")
-		cmd := exec.Command("kubectl", "delete", "ns", namespace)
-		_, _ = utils.Run(cmd)
-	})
-
-	Context("Operator", func() {
-
-		It("should run successfully", func() {
-			var controllerPodName string
-			var err error
-
-			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/thanos-operator:v0.0.1"
-
-			By("building the manager(Operator) image")
-			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
-			_, err = utils.Run(cmd)
-			ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-			By("loading the the manager(Operator) image on Kind")
-			err = utils.LoadImageToKindClusterWithName(projectimage)
-			ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-			By("installing CRDs")
-			cmd = exec.Command("make", "install")
-			_, err = utils.Run(cmd)
-			ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-			By("deploying the controller-manager")
-			cmd = exec.Command("make", "deploy", fmt.Sprintf("IMG=%s", projectimage))
-			_, err = utils.Run(cmd)
-			ExpectWithOffset(1, err).NotTo(HaveOccurred())
-
-			By("validating that the controller-manager pod is running as expected")
-			verifyControllerUp := func() error {
-				// Get pod name
-
-				cmd = exec.Command("kubectl", "get",
-					"pods", "-l", "control-plane=controller-manager",
-					"-o", "go-template={{ range .items }}"+
-						"{{ if not .metadata.deletionTimestamp }}"+
-						"{{ .metadata.name }}"+
-						"{{ \"\\n\" }}{{ end }}{{ end }}",
-					"-n", namespace,
-				)
-
-				podOutput, err := utils.Run(cmd)
-				ExpectWithOffset(2, err).NotTo(HaveOccurred())
-				podNames := utils.GetNonEmptyLines(string(podOutput))
-				if len(podNames) != 1 {
-					return fmt.Errorf("expect 1 controller pods running, but got %d", len(podNames))
-				}
-				controllerPodName = podNames[0]
-				ExpectWithOffset(2, controllerPodName).Should(ContainSubstring("controller-manager"))
-
-				// Validate pod status
-				cmd = exec.Command("kubectl", "get",
-					"pods", controllerPodName, "-o", "jsonpath={.status.phase}",
-					"-n", namespace,
-				)
-				status, err := utils.Run(cmd)
-				ExpectWithOffset(2, err).NotTo(HaveOccurred())
-				if string(status) != "Running" {
-					return fmt.Errorf("controller pod in %s status", status)
-				}
-				return nil
-			}
-			EventuallyWithOffset(1, verifyControllerUp, time.Minute, time.Second).Should(Succeed())
-
-		})
-	})
-
-	Describe("Thanos Receive", Ordered, func() {
+var _ = Describe("e2e tests", func() {
+	Describe("Thanos Receive", func() {
 		routerName := controller.ReceiveRouterNameFromParent(receiveName)
 		ingesterName := controller.ReceiveIngesterNameFromParent(receiveName, hashringName)
 
@@ -293,7 +147,7 @@ var _ = Describe("controller", Ordered, func() {
 
 	})
 
-	Describe("Thanos Query", Ordered, func() {
+	Describe("Thanos Query", func() {
 		Context("When ThanosQuery is created", func() {
 			It("should bring up the thanos query components", func() {
 				cr := &v1alpha1.ThanosQuery{
@@ -405,7 +259,7 @@ var _ = Describe("controller", Ordered, func() {
 		})
 	})
 
-	Describe("Thanos Ruler", Ordered, func() {
+	Describe("Thanos Ruler", func() {
 		Context("When ThanosRuler is created", func() {
 			It("should bring up the rulers components", func() {
 				cr := &v1alpha1.ThanosRuler{
@@ -489,7 +343,7 @@ var _ = Describe("controller", Ordered, func() {
 		})
 	})
 
-	Describe("Thanos Store", Ordered, func() {
+	Describe("Thanos Store", func() {
 		Context("When ThanosStore is created", func() {
 			It("should bring up the store components", func() {
 				cr := &v1alpha1.ThanosStore{
@@ -543,7 +397,7 @@ var _ = Describe("controller", Ordered, func() {
 		})
 	})
 
-	Describe("Thanos Compact", Ordered, func() {
+	Describe("Thanos Compact", func() {
 		Context("When ThanosCompact is created", func() {
 			It("should bring up the compact components", func() {
 				cr := &v1alpha1.ThanosCompact{
@@ -573,7 +427,7 @@ var _ = Describe("controller", Ordered, func() {
 			})
 		})
 	})
-	Describe("Discover Thanos components as targets with Service Monitor Enabled", Ordered, func() {
+	Describe("Discover Thanos components as targets with Service Monitor Enabled", func() {
 		Context("Discover Thanos Components as a target", func() {
 			It("Service Should be present in Up Metric", func() {
 				pods := &corev1.PodList{}
