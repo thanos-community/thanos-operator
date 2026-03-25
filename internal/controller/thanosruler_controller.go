853c573d378dcb33b1d34da675d3f071b228dd0a
Filip <c00301624@setu.ie>
2026-03-25 11:49:09 +0000

    ctrl:ruler: Extend rbac for crud on secrets for stateless ruler remote write config

--- internal/controller/thanosruler_controller.go
+++ internal/controller/thanosruler_controller.go
@@ -106,7 +106,7 @@ func NewThanosRulerReconciler(conf Config, configReloaderImage string, client cl
 // +kubebuilder:rbac:groups=monitoring.thanos.io,resources=thanosrulers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=monitoring.thanos.io,resources=thanosrulers/finalizers,verbs=update
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups="",resources=services;configmaps;serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services;configmaps;serviceaccounts;secrets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=policy,resources=poddisruptionbudgets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=monitoring.coreos.com,resources=prometheusrules,verbs=get;list;watch
 
